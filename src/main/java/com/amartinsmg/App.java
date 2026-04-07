dfcbb6fcf36cccd9c9465019f6898c8902ed8834
amartinsmg <andremindurimg@gmail.com>
2026-04-07 19:49:10 -0300

    Add initial implementation of App and AppTest classes

---
+++ src/main/java/com/amartinsmg/App.java
@@ -0,0 +1,13 @@
+package com.amartinsmg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
