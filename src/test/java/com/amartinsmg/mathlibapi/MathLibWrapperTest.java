9ff89c4745ef4f9ac949aad41bff7bb4be22c31b
amartinsmg <andremindurimg@gmail.com>
2026-04-07 22:44:06 -0300

    test: add tests for logarithm, sphere, cone, prime factors
    - Add test for logarithm function
    - Add test for sphere area calculation
    - Add test for cone volume calculation
    - Add test for prime factors function

--- src/test/java/com/amartinsmg/mathlibapi/MathLibWrapperTest.java
+++ src/test/java/com/amartinsmg/mathlibapi/MathLibWrapperTest.java
@@ -11,9 +11,34 @@ public class MathLibWrapperTest {
     static double[] arr = {4, 5, 8, 13, 13, 15, 16, 18, 20};
 
     @Test
+    public void testLogarithm() {
+        double result = MathLibWrapper.logarithm(65536, 2);
+        assertEquals(16, result, 1e-6);
+    }
+
+    @Test
+    public void testSphereArea() {
+        double result = MathLibWrapper.sphereArea(5);
+        assertEquals(314.159265, result, 1e-6);
+    }
+
+    @Test
+    public void testConeVol() {
+        double result = MathLibWrapper.coneVol(3, 5);
+        assertEquals(47.1238898, result, 1e-6);
+    }
+
+    @Test
     public void testGcd() {
         long result = MathLibWrapper.gcd(88, 55);
         assertEquals(11, result);
+    }
+
+    @Test
+    public void testPrimeFactors() {
+        long[] result = MathLibWrapper.primeFactors(1025640),
+                expected = {2, 2, 2, 3, 3, 5, 7, 11, 37};
+        assertArrayEquals(expected, result);
     }
 
     @Test
