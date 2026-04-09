54e3a208ae4c8201f4ba06d6d6eba0f1dc47d85e
amartinsmg <andremindurimg@gmail.com>
2026-04-09 12:06:18 -0300

    refactor: Simplify FunctionRegistry constructor
    - Removed direct MathService registration
    - Allow external classes to be passed in constructor

--- src/main/java/com/amartinsmg/mathlibapi/api/FunctionRegistry.java
+++ src/main/java/com/amartinsmg/mathlibapi/api/FunctionRegistry.java
@@ -5,17 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.amartinsmg.mathlibapi.schema.annotations.ApiFunction;
-import com.amartinsmg.mathlibapi.service.MathService;
 
 public class FunctionRegistry {
 
     private final Map<String, Method> registry = new HashMap<>();
 
-    public FunctionRegistry() {
-        register(MathService.class);
-    }
-
-    public void register(Class<?> clazz) {
+    public FunctionRegistry(Class<?> clazz) {
         for (Method method : clazz.getDeclaredMethods()) {
             if (method.isAnnotationPresent(ApiFunction.class)) {
                 ApiFunction apiFunc = method.getAnnotation(ApiFunction.class);
