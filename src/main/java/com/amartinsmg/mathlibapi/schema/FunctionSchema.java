9c0ff7ad085a9e977142bd91f9f337f396d9418a
amartinsmg <andremindurimg@gmail.com>
2026-04-09 20:56:55 -0300

    feat(schema): add function schema definition

---
+++ src/main/java/com/amartinsmg/mathlibapi/schema/FunctionSchema.java
@@ -0,0 +1,12 @@
+package com.amartinsmg.mathlibapi.schema;
+
+import java.util.ArrayList;
+
+public class FunctionSchema {
+
+    public String name;
+    public String description;
+    public Object returnType;
+    public ArrayList<ParamSchema> params;
+
+}
