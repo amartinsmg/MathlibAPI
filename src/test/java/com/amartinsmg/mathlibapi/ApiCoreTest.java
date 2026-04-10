16d0d549b91067f552f567f3e47a67090a6a8d6d
amartinsmg <andremindurimg@gmail.com>
2026-04-10 12:42:04 -0300

    test(ApiCore): refactor and add new tests
    - Renamed existing test methods
    - Added test for invalid argument handling
    - Verified `execEngine` double result
    - Verified `returnNormalizer` Map result

--- src/test/java/com/amartinsmg/mathlibapi/ApiCoreTest.java
+++ src/test/java/com/amartinsmg/mathlibapi/ApiCoreTest.java
@@ -1,10 +1,11 @@
 package com.amartinsmg.mathlibapi;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import com.amartinsmg.mathlibapi.api.ApiCore;
@@ -13,21 +14,50 @@ import com.amartinsmg.mathlibapi.service.MathService;
 public class ApiCoreTest {
 
     @Test
-    public void testCreateApiCore() {
+    public void shouldGenerateCore() {
         var core = new ApiCore(MathService.class);
         assertNotNull(core);
     }
 
     @Test
-    public void testEngine() throws Exception {
+    public void shouldThrowsException() throws Exception {
         var core = new ApiCore(MathService.class);
 
-        Map<String, Object> args = new HashMap<>();
+        Map<String, Object> args = Map.of("num", 10.2);
 
-        args.put("degrees", 180.0);
+        RuntimeException ex = assertThrows(
+                RuntimeException.class,
+                () -> core.execEngine("prime-factors", args)
+        );
+        assertEquals("Invalid argument num: expected int64", ex.getMessage());
+    }
 
-        var result = core.engine("deg-to-rad", args);
+    @Test
+    public void testExecEngine() throws Exception {
+        var core = new ApiCore(MathService.class);
+
+        Map<String, Object> args = Map.of("degrees", 180.0);
+
+        var result = core.execEngine("deg-to-rad", args);
+
+        assertInstanceOf(Double.class, result);
 
         assertEquals(3.14159265, (double) result, 1e-6);
+    }
+
+    @Test
+    public void testReturnNormaiizer() throws Exception {
+        var core = new ApiCore(MathService.class);
+
+        Map<String, Object> args = Map.of("a-x", 0.0, "a-y", 0.0, "b-x", 3.0, "b-y", 5.0);
+
+        var result = core.execEngine("midpoint", args);
+
+        assertInstanceOf(Map.class, result);
+
+        Map<?, ?> map = (Map<?, ?>) result;
+
+        assertEquals(1.5, (double) map.get("x"), 1e-6);
+        assertEquals(2.5, (double) map.get("y"), 1e-6);
     }
 }
