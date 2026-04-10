db123a60f3600769c70ca9ba781187ccf008f09b
amartinsmg <andremindurimg@gmail.com>
2026-04-10 12:00:01 -0300

    feat(schema): add namespace to function schema

--- src/main/java/com/amartinsmg/mathlibapi/schema/FunctionSchema.java
+++ src/main/java/com/amartinsmg/mathlibapi/schema/FunctionSchema.java
@@ -6,6 +6,7 @@ public class FunctionSchema {
 
     public String name;
     public String description;
+    public String namespace;
     public Object returnType;
     public ArrayList<ParamSchema> params;
 
