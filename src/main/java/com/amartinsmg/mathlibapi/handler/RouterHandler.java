d4af244f6d52fabbb291ef491b5fe544c64a0ec9
amartinsmg <andremindurimg@gmail.com>
2026-04-10 18:47:32 -0300

    feat(handler): add RouterHandler for request routing
    - Encapsulates HTTP method and handler logic
    - Handles method mismatch with 405 response
    - Catches ApiException for custom error responses
    - Catches generic exceptions for 500 errors

---
+++ src/main/java/com/amartinsmg/mathlibapi/handler/RouterHandler.java
@@ -0,0 +1,43 @@
+package com.amartinsmg.mathlibapi.handler;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.amartinsmg.mathlibapi.core.exceptions.ApiException;
+import com.amartinsmg.mathlibapi.utils.JsonUtils;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class RouterHandler {
+
+    private final String method;
+    private final HttpHandler handler;
+
+    public RouterHandler(String method, HttpHandler handler) {
+        this.method = method;
+        this.handler = handler;
+    }
+
+    public void handle(HttpExchange ex) throws IOException {
+        if (!ex.getRequestMethod().equals(method)) {
+            ex.sendResponseHeaders(405, -1);
+            ex.close();
+            return;
+        }
+        try {
+            handler.handle(ex);
+        } catch (ApiException e) {
+            String error = JsonUtils.toJson(Map.of("error", e.getMessage()));
+
+            ex.getResponseHeaders().add("Content-Type", "application/json");
+            ex.sendResponseHeaders(e.getStatus(), error.length());
+            ex.getResponseBody().write(error.getBytes());
+            ex.close();
+        } catch (Exception e) {
+            String errorJson = "{\"error\":\"Internal server error\"}";
+            ex.sendResponseHeaders(500, errorJson.length());
+            ex.getResponseBody().write((errorJson.getBytes()));
+            ex.close();
+        }
+    }
+}
