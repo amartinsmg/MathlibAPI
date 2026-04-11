5182ea31424b23b089272eb6681d2ed305d6e527
amartinsmg <andremindurimg@gmail.com>
2026-04-11 13:06:13 -0300

    feat(core): add ConversionException class

---
+++ src/main/java/com/amartinsmg/mathlibapi/core/exceptions/ConversionException.java
@@ -0,0 +1,8 @@
+package com.amartinsmg.mathlibapi.core.exceptions;
+
+public class ConversionException extends RuntimeException {
+
+    public ConversionException(String m) {
+        super(m);
+    }
+}
